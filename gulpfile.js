dedc269132c8d97c4822642c3609ab0268abba95
matxx <matxx.webmaster@gmail.com>
2017-07-15 10:11:33 +0200

    all tasks in gulfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -26,3 +26,44 @@ gulp.task('minify:css', function() {
              .pipe(rename({ suffix: ".min" }))
              .pipe(gulp.dest('dist/css'))
 })
+
+const nodemw = require('nodemw')
+const fs = require('fs')
+
+function logAndExecute(callback) {
+  return function (err, data) {
+    if (err) {
+      console.log(err)
+    }
+    else {
+      callback(data)
+    }
+  }
+}
+
+gulp.task('upload:css', function() {
+  let bot = new nodemw({
+    protocol: 'http',
+    server: process.env.WIKI_DOMAIN_EU_FR,
+    path: '',
+    debug: true
+  })
+  const cssPath = path.join(__dirname, 'dist', 'css', 'all.min.css')
+
+  bot.logIn(process.env.WIKI_BOT_USERNAME, process.env.WIKI_BOT_PASSWORD, logAndExecute(readFile))
+
+  function readFile() {
+    fs.readFile(cssPath, logAndExecute(uploadCss))
+  }
+
+  function uploadCss(css) {
+    bot.edit('MediaWiki:Test.css', css, 'update CSS', '1', function (err, data) {
+      if (err) {
+        console.log(err)
+      }
+      else {
+        console.log(data)
+      }
+    })
+  }
+})
